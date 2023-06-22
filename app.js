94e1728382a22132006c1d025d915f5495c87491
Alcal <alcal.rs221@gmail.com>
2023-06-22 10:50:47 +0200

    Constructeur app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+class InfinitePagination {
+
+    #endpoint
+    #template
+    #target
+    #elements
+
+    /**
+     * 
+     * @param {HTMLElement} element 
+     */
+    constructor(element) {
+        this.#endpoint = element.dataset.endpoint
+        this.#template = element.dataset.template
+        this.#target = element.dataset.target
+        this.#elements = element.dataset.elements
+    }
+}
+
+document
+    .querySelectorAll('.js-infinite-pagination')
+    .forEach(el => new InfinitePagination(el))
