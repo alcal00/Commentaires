ddbfc28b4f47efaca4b793eb61e414b93beefc5d
Alcal <alcal.rs221@gmail.com>
2023-06-22 15:50:58 +0200

    Injection de valeur dans les templates + loading

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ class InfinitePagination {
     #elements
     /**@type {IntersectionObserver} */
     #observer
+    /**@type {boolean} */
+    #loading = false
 
     /**
      * 
@@ -21,10 +23,10 @@ class InfinitePagination {
         this.#endpoint = element.dataset.endpoint
         this.#template = document.querySelector(element.dataset.template)
         this.#target = document.querySelector(element.dataset.target)
-        this.#elements = element.dataset.elements 
+        this.#elements = JSON.parse(element.dataset.elements)
         this.#observer = new IntersectionObserver((entries) => {
-            for(const entry of entries){
-                if (entry.isIntersecting){
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
                     this.#loadMore()
                 }
             }
@@ -32,12 +34,25 @@ class InfinitePagination {
         this.#observer.observe(element)
     }
 
-    async #loadMore(){
-         const comments = await fetchJSON(this.#endpoint)
-         for(const comment of comments){
-            const commentElement = this.#template.content.cloneNode(true)
+    async #loadMore() {
+        if (this.#loading) {
+            return
+        }
+        this.#loading = true
+        const comments = await fetchJSON(this.#endpoint)
+        for (const comment of comments) {
+            const commentElement = this.#template.content.cloneNode(true) // template vide
+            for (const [key, selector] of Object.entries(this.#elements)) {
+                console.log({ key, selector })
+                commentElement.querySelector(selector).innerText = comment[key] 
+                /* ca recupere le selector qui est soit js-username ou js-content on le modifie
+                avec comment[key]. comment est un commentaire parmi les commentaires qu'on parcours
+                key est une valeur soit name soit body. Donc comment[key] on recupere la valeur 
+                name(string) et de body(string) dans un commentaire*/
+            }
             this.#target.append(commentElement)
-         }
+        }
+        this.#loading = false
     }
 }
 
